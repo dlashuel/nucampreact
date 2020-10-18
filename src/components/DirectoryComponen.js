0164b5ac6eb3573956f80e9ff08c5e45a93c2761
dlashuel <dlashuel@gmail.com>
2020-10-18 12:56:29 -0700

    create campsite and directory components

---
+++ src/components/DirectoryComponen.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import CampsiteInfo from './CampsiteInfoComponent.js';
+
+class Directory extends Component {
+    constructor(props){
+        console.log(props)
+        super(props);
+        this.state = {
+          selectedCampsite: null
+        };
+      }
+      onCampsiteSelect(campsite){
+          this.setState({selectedCampsite: campsite})
+      }
+      renderSelectedCampsite(campsite) {
+          if (campsite) {
+              return(
+                  <Card>
+                      <CardImg top src={campsite.image} alt={campsite.name} />
+                      <CardBody>
+                          <CardTitle>{campsite.name}</CardTitle>
+                          <CardText>{campsite.description}</CardText>
+                      </CardBody>
+                  </Card>
+              )  
+          }return <div />;
+      }
+    render(){
+        const directory = this.props.campsites.map(campsite => {
+            return(
+                <div key={campsite.id} className="col-md-5 m1">
+                    <Card onClick = {()=> this.onCampsiteSelect(campsite)}>
+                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{campsite.name}</CardTitle>
+                            <CardText></CardText>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        }); 
+        return(
+            <div className="container">
+                <div className="row">
+                    {directory}
+                </div>
+                <div>
+                    <CampsiteInfo campsite={this.state.selectedCampsite}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Directory;
